230a8c10fcf875bbb9570adc72f28593bcbc4e15
velibeyli <valibayli91@gmail.com>
2021-12-09 22:39:59 +0400

    Bank app created and added

---
+++ KapitalBankApp/KapitalBankApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapitalBankApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApp();
+            MobileApp();
+            API();
+            Console.ReadLine();
+        }
+        public static void WebApp()
+        {
+            Console.WriteLine("This is WebApplication.");
+        }
+        public static void MobileApp()
+        {
+            Console.WriteLine("This is MobileApp.");
+        }
+        public static void API()
+        {
+            Console.WriteLine("This is API.");
+        }
+    }
+}
